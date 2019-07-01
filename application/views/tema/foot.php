2a269eb27f7dfba441681550f72213187920adcb
citurrieta <citurrieta@bcncons.com>
2019-07-01 11:41:33 -0400

    Se cambia firma del footer y se edita index a produccion

--- application/views/tema/foot.php
+++ application/views/tema/foot.php
@@ -4,7 +4,8 @@
             &copy; Copyright 2018 <?= $this->parametrosInicio['software_nombre'] . " " . $this->configLocal['software_version'] . " (" . $this->configLocal['software_date'] . ")" ?> - Todos los derechos reservados BCN Consultores Ltda. - Server <?= $this->configLocal['_APP_SERVER'] ?>
         </div>
         <div id="firma">
-            <img src="<?= base_url() ?>include/imagenes/firma-bcn.png" width="128" height="14" alt="firma"/>
+            <!--<img src="<?= base_url() ?>include/imagenes/firma-bcn.png" width="128" height="14" alt="firma"/>-->
+			Version creada por Carlos Iturrieta
         </div>
     </div>
 </div>
